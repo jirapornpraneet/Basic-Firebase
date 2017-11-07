ac454501ea8160d53f38ef3aee0d5a1fb4a09094
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-11-07 13:28:20 +0700

    กำหนด customClass  เป็น LoginViewController และ เชื่อมโยง UITextField, UIButton ไปที่หน้า LoginViewController

--- app-nut-firebase/LoginViewController.swift
+++ app-nut-firebase/LoginViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +24,5 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
