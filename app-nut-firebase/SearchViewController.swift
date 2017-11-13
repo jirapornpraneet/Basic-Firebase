cbc7fbe066c69a7040164d0fc2aedcf9aaab57d1
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-11-13 17:57:59 +0700

    เพิ่มชื่อ title navigationBar ในหน้า SerachViewController

--- app-nut-firebase/SearchViewController.swift
+++ app-nut-firebase/SearchViewController.swift
@@ -16,20 +16,8 @@ class SearchViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.topItem?.title = "Search"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
