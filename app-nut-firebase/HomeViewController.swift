7db29041411f9c34ae1624b6b98b121a64672aab
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-11-13 17:51:07 +0700

    เพิ่มชื่อ หัวข้อของ navigationBar

--- app-nut-firebase/HomeViewController.swift
+++ app-nut-firebase/HomeViewController.swift
@@ -16,8 +16,8 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.topItem?.title = "Home"
     }
 }
