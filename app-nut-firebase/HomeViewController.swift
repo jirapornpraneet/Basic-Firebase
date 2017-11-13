5241bae3fadceb7785a89ebc1597572d8373a9d5
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-11-13 14:23:17 +0700

    เพิ่ม TabBar ในหน้า HomeViewController

--- app-nut-firebase/HomeViewController.swift
+++ app-nut-firebase/HomeViewController.swift
@@ -9,14 +9,119 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import AVFoundation
+import AZTabBar
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, AZTabBarDelegate {
+
+    var tabController: AZTabBarController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var icons = [UIImage]()
+        icons.append(#imageLiteral(resourceName: "ic_home_outline"))
+        icons.append(#imageLiteral(resourceName: "ic_search_outline"))
+        icons.append(#imageLiteral(resourceName: "ic_camera_outline"))
+        icons.append(#imageLiteral(resourceName: "ic_heart_outline"))
+        icons.append(#imageLiteral(resourceName: "ic_account_outline"))
+
+        var sIcons = [UIImage]()
+        sIcons.append(#imageLiteral(resourceName: "ic_home"))
+        sIcons.append(#imageLiteral(resourceName: "ic_search"))
+        sIcons.append(#imageLiteral(resourceName: "ic_camera"))
+        sIcons.append(#imageLiteral(resourceName: "ic_heart"))
+        sIcons.append(#imageLiteral(resourceName: "ic_account"))
+
+        tabController = AZTabBarController.insert(into: self, withTabIcons: icons, andSelectedIcons: sIcons)
+        tabController.delegate = self
+
+        customTabBarController()
+
     }
-    
+
+    func customTabBarController() {
+        let myHomeViewController = UIStoryboard(name: "Main",
+                                                bundle: nil)
+            .instantiateViewController(withIdentifier: "HomeViewController")
+        let mySearchViewController = UIStoryboard(name: "Main",
+                                                  bundle: nil)
+            .instantiateViewController(withIdentifier: "SearchViewController")
+        let myCameraViewController = UIStoryboard(name: "Main",
+                                                  bundle: nil)
+            .instantiateViewController(withIdentifier: "CameraViewController")
+        let myHeartViewController  = UIStoryboard(name: "Main",
+                                                  bundle: nil)
+            .instantiateViewController(withIdentifier: "HeartViewController")
+        let myAccountViewController = UIStoryboard(name: "Main",
+                                                   bundle: nil)
+            .instantiateViewController(withIdentifier: "AccountViewController")
+
+        tabController.setViewController(myHomeViewController, atIndex: 0)
+        tabController.setViewController(mySearchViewController, atIndex: 1)
+        tabController.setViewController(myCameraViewController, atIndex: 2)
+        tabController.setViewController(myHeartViewController, atIndex: 3)
+        tabController.setViewController(myAccountViewController, atIndex: 4)
+
+        tabController?.setAction(atIndex: 4) {
+            print("Index 4")
+        }
+        tabController?.setAction(atIndex: 3) {
+            print("Index 3")
+        }
+        tabController?.setAction(atIndex: 2) {
+            print("Index 2")
+        }
+        tabController?.setAction(atIndex: 1) {
+            print("Index 1")
+        }
+        tabController?.setAction(atIndex: 0) {
+            print("Index 0")
+        }
+
+        tabController.defaultColor = UIColor(red: 0.95, green: 0.58, blue: 0.58, alpha: 1.0)
+        tabController.selectedColor = UIColor(red: 0.99, green: 0.33, blue: 0.44, alpha: 1.0)
+        tabController.highlightColor = .white
+        tabController.highlightedBackgroundColor = .green
+        tabController.buttonsBackgroundColor = .white
+        tabController.selectionIndicatorColor = .green
+        tabController.selectionIndicatorHeight = 0
+        tabController.separatorLineColor = .black
+        tabController.separatorLineVisible = false
+        tabController.animateTabChange = true
+    }
+
+    override var childViewControllerForStatusBarStyle: UIViewController? {
+        return tabController
+    }
+
+    func tabBar(_ tabBar: AZTabBarController, statusBarStyleForIndex index: Int) -> UIStatusBarStyle {
+        return (index % 2) == 0 ? .default : .lightContent
+    }
+
+    func tabBar(_ tabBar: AZTabBarController, shouldLongClickForIndex index: Int) -> Bool {
+        return true//index != 2 && index != 3
+    }
+
+    func tabBar(_ tabBar: AZTabBarController, shouldAnimateButtonInteractionAtIndex index: Int) -> Bool {
+        return false
+    }
+
+    func tabBar(_ tabBar: AZTabBarController, didMoveToTabAtIndex index: Int) {
+        print("didMoveToTabAtIndex \(index)")
+    }
+
+    func tabBar(_ tabBar: AZTabBarController, didSelectTabAtIndex index: Int) {
+        print("didSelectTabAtIndex \(index)")
+    }
+
+    func tabBar(_ tabBar: AZTabBarController, willMoveToTabAtIndex index: Int) {
+        print("willMoveToTabAtIndex \(index)")
+    }
+
+    func tabBar(_ tabBar: AZTabBarController, didLongClickTabAtIndex index: Int) {
+        print("didLongClickTabAtIndex \(index)")
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         self.navigationItem.setHidesBackButton(true, animated: false)
     }
