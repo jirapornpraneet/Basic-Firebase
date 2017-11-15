66d3ebff116448be391d811a2fcb5ea310d00ac1
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-11-15 14:22:22 +0700

    กำหนดการทำงานเมื่อกดปุ่ม upload image profile

--- app-nut-firebase/SingUpViewController.swift
+++ app-nut-firebase/SingUpViewController.swift
@@ -11,7 +11,8 @@ import Firebase
 import FirebaseAuth
 import FirebaseDatabase
 
-class SingUpViewController: UIViewController, UITextFieldDelegate {
+class SingUpViewController: UIViewController, UITextFieldDelegate,
+UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var firstnameTextField: UITextField!
     @IBOutlet weak var lastnameTextField: UITextField!
@@ -20,11 +21,13 @@ class SingUpViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var singUpButton: UIButton!
+    @IBOutlet weak var profileImageView: UIImageView!
 
-    var referense: DatabaseReference?
     var handle: DatabaseHandle?
     var genderList: [String] = ["Male", "Female"]
     var genderName: String = ""
+    var databaseReference = Database.database().reference()
+    var storageReference = Storage.storage().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -106,13 +109,126 @@ class SingUpViewController: UIViewController, UITextFieldDelegate {
     }
 
     func addDatabaseReference() {
-        referense = Database.database().reference()
-        referense?.child("users").child("firstname").childByAutoId().setValue(firstnameTextField.text)
-        referense?.child("users").child("lastname").childByAutoId().setValue(lastnameTextField.text)
-        referense?.child("users").child("gender").childByAutoId().setValue(genderName)
-        referense?.child("users").child("email").childByAutoId().setValue(emailTextField.text)
-        referense?.child("users").child("password").childByAutoId().setValue(passwordTextField.text)
-        referense?.child("users").child("confirmPassword").childByAutoId().setValue(confirmPasswordTextField.text)
+        databaseReference
+            .child("users").child("firstname")
+            .childByAutoId()
+            .setValue(firstnameTextField.text)
+        databaseReference
+            .child("users")
+            .child("lastname")
+            .childByAutoId()
+            .setValue(lastnameTextField.text)
+        databaseReference
+            .child("users")
+            .child("gender")
+            .childByAutoId()
+            .setValue(genderName)
+        databaseReference
+            .child("users")
+            .child("email")
+            .childByAutoId()
+            .setValue(emailTextField.text)
+        databaseReference
+            .child("users")
+            .child("password")
+            .childByAutoId()
+            .setValue(passwordTextField.text)
+        databaseReference
+            .child("users")
+            .child("confirmPassword")
+            .childByAutoId()
+            .setValue(confirmPasswordTextField.text)
+    }
+
+    func setProfileImageView() {
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.width/2
+        profileImageView.clipsToBounds = true
+        if let uid = Auth.auth().currentUser?.uid {
+            databaseReference
+                .child("users")
+                .child(uid)
+                .observeSingleEvent(of:
+                    .value, with: { (snapshot) in
+                        if let dict = snapshot.value as? [String: AnyObject] {
+                            if let profileImageURL = dict["pic"] as? String {
+                                let url = URL(string: profileImageURL)
+                                URLSession.shared.dataTask(with: url!,
+                                                           completionHandler: { (data, _, error) in
+                                                            if error != nil {
+                                                                print(error!)
+                                                                return
+                                                            }
+                                                            DispatchQueue.main.async {
+                                                                self.profileImageView.image = UIImage(data: data!)
+                                                            }
+                                }).resume()
+                            }
+                        }
+                })
+        }
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        var selectedImageFromPicker: UIImage?
+
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        if let selectedImage = selectedImageFromPicker {
+            profileImageView.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func uploadImageButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+
+    func saveProfileImageView() {
+        let imageName = NSUUID().uuidString
+        let storedImage = storageReference.child("profileUserImages").child(imageName)
+
+        if let uploadData = UIImagePNGRepresentation(self.profileImageView.image!) {
+            storedImage
+                .putData(uploadData,
+                         metadata: nil,
+                         completion: { (_, error) in
+                            if error != nil {
+                                print(error!)
+                                return
+                            }
+                            storedImage.downloadURL(completion: { (url, error) in
+                                if error != nil {
+                                    print(error!)
+                                    return
+                                }
+                                if let urlText = url?.absoluteString {
+                                    self.databaseReference
+                                        .child("users")
+                                        .child((Auth
+                                            .auth()
+                                            .currentUser?
+                                            .uid)!)
+                                        .updateChildValues(["pic": urlText],
+                                                           withCompletionBlock: { (error, _) in
+                                                            if error != nil {
+                                                                print(error!)
+                                                                return
+                                                            }
+                                        })
+                                }
+                            })
+                })
+        }
     }
 
     @IBAction func singUpClicked(_ sender: Any) {
