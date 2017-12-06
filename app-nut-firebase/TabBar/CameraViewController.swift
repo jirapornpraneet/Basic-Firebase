9be16050f97c6f375fc4c0b34ad081fcbda50d8b
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2017-12-06 11:44:10 +0700

    กำหนดให้ฟังก์ชั่น takePhoto ให้สามารถ ถ่ายรูปได้

--- app-nut-firebase/TabBar/CameraViewController.swift
+++ app-nut-firebase/TabBar/CameraViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
     
     @IBOutlet var imageView: UIImageView!
 
@@ -17,10 +18,27 @@ class CameraViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func takePhoto(_ sender: AnyObject){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleToFill
+            imageView.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     override func viewWillAppear(_ animated: Bool) {
